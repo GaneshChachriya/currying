2e7a4c8418c90edd2e099e99d876b0deccc0bc3a
ganesh chachriya <g.chachriya11@gmail.com>
2022-07-18 21:29:20 +0530

    call back function

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('clickMe').addEventListener('click', function abc(){
+console.log("clickMe");
+})
+document.addEventListener('DOMContentLoaded', init);
+function init() {
+	console.log('The page successfully loaded!');
+}
